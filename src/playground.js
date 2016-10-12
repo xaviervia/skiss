7aaa2e29438aa65a42d271ac8bc96325b0e33495
Xavier Via <fernando.via@gmail.com>
2016-10-12 22:06:08 +0200

    Synthetic runtime components

--- src/playground.js
+++ src/playground.js
@@ -1,36 +1,59 @@
-import React from 'react'
-import {introspect} from 'patch-react-proptypes-add-introspection'
-import {render} from 'react-dom'
-import {mapObjIndexed} from 'ramda'
-import Editor from 'components/Editor'
+import React, {PropTypes} from 'react'
+import asCode from 'lib/asCode'
+import { render } from 'react-dom'
 
-const {default: dictionary} = require('dictionary')
-
-const propTypesDictionary = mapObjIndexed(
-  (component) => component.propTypes && introspect(component.propTypes),
-  dictionary
-)
-
-const data = {
-  type: 'FlatButton',
-  props: {
-    href: 'http://google.com',
-    primary: true
-  },
-  children: [
-    {
-      type: 'text',
-      props: {
-        content: 'Google'
-      }
+function synteticComponent (nodeTree, propTypes, name) {
+  const SynteticComponent = (props) => {
+    console.log(props)
+    const {children, ...otherProps} = props
+    console.log(children)
+    console.log(otherProps)
+    if (children == null) {
+      console.log('here')
+      return React.cloneElement(nodeTree, otherProps)
+    } else {
+      return React.cloneElement(
+        nodeTree,
+        otherProps,
+        [nodeTree.props.children, children]
+      )
     }
-  ]
+  }
+
+  SynteticComponent.displayName = name
+  SynteticComponent.propTypes
+
+  return SynteticComponent
 }
 
+const comp = <p style={{background: 'red', width: '10px', height: '10px'}}>
+  <a href='http://lelele'>
+    <span>aaasdfadf</span>
+  </a>
+</p>
+
+const MyP = synteticComponent(comp, { children: PropTypes.node }, 'MyP')
+
+const renderedMyP = <MyP><blockquote>whats up</blockquote></MyP>
+
 render(
-  <Editor
-    data={data}
-    propTypesDictionary={propTypesDictionary}
-  />,
+  <div>
+    <MyP title='text' />
+    <MyP><blockquote>whats up</blockquote></MyP>
+    <hr />
+    <pre>
+      {asCode(<MyP />)}
+    </pre>
+
+    <hr />
+    <pre>
+      {asCode(renderedMyP)}
+    </pre>
+
+    <hr />
+    <pre>
+      {asCode(comp)}
+    </pre>
+  </div>,
   document.getElementById('skiss')
 )
